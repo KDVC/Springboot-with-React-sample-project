1e734e916565fd7bc47c904c73bf054ae7d0d5be
viraj  wickramasinghe <viraj.wickramasinghe@axiatadigitallabs.com>
2020-03-02 17:53:33 +0530

    add domain class

---
+++ src/main/java/io/agileinteligence/ppmtool/domain/Project.java
@@ -0,0 +1,9 @@
+package io.agileinteligence.ppmtool.domain;
+
+/**
+ * @author : Viraj Wickramasinghe
+ * @email : viraj.wickramasinghe@axiatadigitallabs.com
+ * @date : 3/2/2020
+ */
+public class Project {
+}
